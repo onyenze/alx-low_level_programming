7d8d7587240d068d3ba5f4d66972cd8900e2a4df
onyenze <chibuezeonyenze123@gmail.com>
2022-06-03 13:01:03 -0700

    cdf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - main block
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	char h = 'a';
+
+	while (h <= 'z')
+	{
+		putchar(h);
+		h++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
