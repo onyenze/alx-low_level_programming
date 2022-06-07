379a7b2f231f6ca28a6c9fb4408fef91a222dc7f
onyenze <chibuezeonyenze123@gmail.com>
2022-06-07 15:41:39 -0700

    rtr

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,21 +2,28 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _abs - value absolute
  *
+ *@n: The int to print
  * Return: Always 0.
  */
-int main(void)
-{
-	int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+}
+
 }
