5c2ab2e66134ed63ecb3d3a2740f21ef88b54ef4
onyenze <chibuezeonyeze123@gmail.com>
2022-06-30 06:45:37 -0700

    push

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,64 +2,64 @@
 #include <stdlib.h>
 
 /**
- *  * _print - moves a string one place to the left and prints the string
- *   * @str: string to move
- *    * @l: size of string
- *     *
- *      * Return: void
- *       */
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
 void _print(char *str, int l)
 {
-		int i, j;
+	int i, j;
 
-			i = j = 0;
-				while (i < l)
-						{
-									if (str[i] != '0')
-													j = 1;
-											if (j || i == l - 1)
-															_putchar(str[i]);
-													i++;
-														}
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
 
-					_putchar('\n');
-						free(str);
+	_putchar('\n');
+	free(str);
 }
 
 /**
- *  * mul - multiplies a char with a string and places the answer into dest
- *   * @n: char to multiply
- *    * @num: string to multiply
- *     * @num_index: last non NULL index of num
- *      * @dest: destination of multiplication
- *       * @dest_index: highest index to start addition
- *        *
- *         * Return: pointer to dest, or NULL on failure
- *          */
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-		int j, k, mul, mulrem, add, addrem;
+	int j, k, mul, mulrem, add, addrem;
 
-			mulrem = addrem = 0;
-				for (j = num_index, k = dest_index; j >= 0; j--, k--)
-						{
-									mul = (n - '0') * (num[j] - '0') + mulrem;
-											mulrem = mul / 10;
-													add = (dest[k] - '0') + (mul % 10) + addrem;
-															addrem = add / 10;
-																	dest[k] = add % 10 + '0';
-																		}
-					for (addrem += mulrem; k >= 0 && addrem; k--)
-							{
-										add = (dest[k] - '0') + addrem;
-												addrem = add / 10;
-														dest[k] = add % 10 + '0';
-															}
-						if (addrem)
-								{
-											return (NULL);
-												}
-							return (dest);
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
 }
 /**
  * check_for_digits - checks the arguments to ensure they are digits
