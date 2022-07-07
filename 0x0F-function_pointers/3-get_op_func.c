f9ef76abc3bbfd5e1667b386eb240dbf3027da75
onyenze <chibuezeonyeze123@gmail.com>
2022-07-07 07:14:03 -0700

    something

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,52 +1,31 @@
+#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * op_add - returns the sum of a and b
- * @a: integer
- * @b: integer
- * Return: sum of a and b
+ * get_op_func - elects the correct function to perform the operation
+ * asked by the user
+ * @s: operator passed as argument
+ * Return: a pointer to the function that corresponds to the operator
+ * given as a parameter
+ * NULL if operator doesn't match
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
- * op_sub - returns the difference of a and b
- * @a: integer
- * @b: integer
- * Return: the difference of a and b
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - returns the product of a and b
- * @a: integer
- * @b: integer
- * Return: the product of a and b
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div -  returns the result of the division of a by b
- * @a: integer
- * @b: integer
- * Return: the result of the division of a by b
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - returns the remainder of the division of a by b
- * @a: integer
- * @b: integer
- * Return: the remainder of the division of a by b
- */
-int op_mod(int a, int b)
-{
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
